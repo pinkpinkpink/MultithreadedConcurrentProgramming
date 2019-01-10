a96c9222a1f10009025cfe441150c5d1788facb5
wangyifan <15638515778@163.com>
2019-01-10 12:18:01 +0800

    edit StrToInteger

--- src/main/java/org/thread/com/basement/StringToInteger.java
+++ src/main/java/org/thread/com/basement/StringToInteger.java
@@ -6,18 +6,18 @@ package src.main.java.org.thread.com.basement;
  * @author wangyifan
  * @create 2019-01-10 10:57
  */
-public class StringToInteger {
-    String str;
+public class StringToInteger<str> {
+    String str = "yellow";
     int count = 10;
-    if(str == null)
-    {
+    int num = 11;
+    if(str == null){
         return 0;
     }
-    else if(str != 0){
-        count = count * 10 + str - '0';
-        ++str;
+    try{
+        int count = Integer.parseInt(str);
+        Integer num = Integer.valueOf(str);
+    }catch(NumberFormatException e){
 
     }
-    return count;
 
 }
